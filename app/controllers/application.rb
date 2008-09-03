caec421314126af573fed1124d7806a16c5c8d88
Turner King <tking@obtiva.com>
2008-09-03 15:06:20 -0500

    Reordering methods

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -7,16 +7,19 @@
 # Visit http://www.pragmaticprogrammer.com/titles/mmfacer for more book information.
 #---
 class ApplicationController < ActionController::Base
-  attr_accessor :current_user
-  helper_attr :current_user
-  before_filter :set_current_user
   helper :all # include all helpers, all the time
   
-  def set_current_user
-    self.current_user = User.for(facebook_session.user.to_i)
-  end
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery  :secret => 'a7cabcdf1499df9ded55d8a3797d9387'
   ensure_authenticated_to_facebook
+  
+  helper_attr :current_user
+  
+  attr_accessor :current_user
+  before_filter :set_current_user
+  
+  def set_current_user
+    self.current_user = User.for(facebook_session.user.to_i)
+  end
 end
